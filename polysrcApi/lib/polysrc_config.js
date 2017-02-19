e218ceeb7337994faf9bc6cf7f6e866164ca5a5b
Grant Collins <grantcollins@grants-mbp.attlocal.net>
2017-02-19 12:31:46 -0800

    update

--- polysrcApi/lib/polysrc_config.js
+++ polysrcApi/lib/polysrc_config.js
@@ -2,5 +2,4 @@
 
 Object.defineProperty(exports, "__esModule", {
   value: true
-});
-var DB_URI = exports.DB_URI = 'mongodb://grantcol:weezybaby21@ds137759.mlab.com:37759/polysrc';
+});var DB_URI = exports.DB_URI = 'mongodb://grantcol:weezybaby21@ds137759.mlab.com:37759/polysrc';
